2ff172e1b5a3224e8830748d5a54c1c7c7e2bc12
kurtw555 <kurt.wolfe@gmail.com>
2018-07-20 15:29:29 -0400

    Update to conda install

--- requirements.py
+++ requirements.py
@@ -4,37 +4,66 @@ import json
 import subprocess
 from subprocess import PIPE
 
+#Usage
+#Create a new conda environment or activate an existing one
+#conda create --name myenv
+#conda activate myenv
+#run this script in the folder with the requirements.txt file
+
+
+#set this variable to reflect your conda install
+conda_path = "C:\\Users\\KWOLFE\\AppData\\Local\\Continuum\\anaconda3\\Scripts"
+
+#set to environment you want to create
+env = "qed"
+
 def run_requirements(env):
     with open("requirements.txt") as f_requirements:
         lines = []
         for line in f_requirements:
+            #string out comments
+            loc = line.find("#")
+            if loc > 1:
+                line = line[0:loc].strip()
             lines.append(line)
 
 
     with open("requirements.log","w") as f_log:
 
-        cmd = "conda env list --json"
-        result = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
-        (outp, err) = result.communicate()
-        soutp = outp.decode('utf8')
-        serr = err.decode('utf8')
-        data = json.loads(soutp)
-        pip_path = ""
-        envs = data["envs"]
-        for env_path in envs:
-            if env in env_path.lower():
-                pip_path = env_path
-                pip_path = os.path.join(pip_path, "scripts", "pip")
-                break
+        #Check to see if the environment exists
+        #cmd = "conda env list --json"
+        #cmd = os.path.join(conda_path, cmd)
+        #result = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
+        #(outp, err) = result.communicate()
+        #soutp = outp.decode('utf8')
+        #serr = err.decode('utf8')
+        #print(serr)
+        #data = json.loads(soutp)
+        
+        #If the environment exists, get the pip path
+        #pip_path = ""
+        #env_exists = False
+        #envs = data["envs"]
+        #for env_path in envs:
+        #    if env in env_path.lower():
+        #        pip_path = env_path
+        #        env_exists = True
+        #        pip_path = os.path.join(pip_path, "scripts", "pip")
+        #        break
+
+        #if env_exists == False:
+        #    cmd = "conda create --name " + env
+        #    execute_cmd(cmd, f_log)
 
 
         #conda install pip in the env
-        cmd_conda = "conda install -y --json -n {} {}"
-        cmd = cmd_conda.format(env, "pip")
-        if execute_cmd(cmd, f_log) == False:
-            log_msg(f_log, "Unable to install pip in "+ env)
-            log_msg(f_log, "Exiting")
-            return
+        #dont need to do this
+        #cmd_conda = "conda install -y --json -n {} {}"
+        #cmd = cmd_conda.format(env, "pip")
+        #if execute_cmd(cmd, f_log) == False:
+        #    log_msg(f_log, "Unable to install pip in "+ env)
+        #    log_msg(f_log, "Exiting")
+        #    return
 
 
         for line in lines:
@@ -52,52 +81,14 @@ def run_requirements(env):
 
             #try pip install - make sure use pip from env            
             cmd_pip = "{} install {}"
-            cmd = cmd_pip.format(pip_path, line)            
+            cmd = cmd_pip.format("pip,exe", line)            
             if execute_cmd(cmd, f_log) == True:
                 continue
 
 
-
-
-            #log_msg(f_log, "Attempting: conda install -y --json -n " + env + " " + line)
-            #f_log.write("Attempting conda install: " + line)
-            #f_log.flush()
-            #result = subprocess.Popen("conda install -y --json -n " + env + " " + line, stdout=PIPE, stderr=PIPE)
-            #(outp, err) = result.communicate()
-            #outp = result.stdout.read()
-            #soutp = outp.decode('utf8')
-            #serr = err.decode('utf8') 
-            #print(soutp)
-            #if "error" in soutp.lower() or "fail" in soutp.lower():
-            #    log_msg(f_log, "Failure: " +  "conda install -y --json -n " + env + " " + line)
-            #    log_msg(f_log, "Attempting: conda install -y --json -c conda-forge -n " + env + " " + line)
-            #    result = subprocess.Popen("conda install -y -c conda-forge -n " + env + " " + line, stdout=PIPE, stderr=PIPE)
-            #    (outp, err) = result.communicate()
-            #    soutp = outp.decode('utf8')
-            #    serr = err.decode('utf8') 
-            #    print(soutp)
-            #    if "error" in soutp.lower() or "fail" in soutp.lower():
-            #        log_msg(f_log, "Failure: conda install -y --json -c conda-forge -n " + env + " " + line) 
-            #        result.terminate()  
-            #        log_msg(f_log, "Attempting: pip install " + line)                
-            #        result = subprocess.Popen("pip install "+ line, stdout=PIPE, stderr=PIPE)
-            #        (outp, err) = result.communicate()
-            #        soutp = outp.decode('utf8')
-            #        serr = err.decode('utf8') 
-            #        print(soutp)
-            #        if "error" in soutp.lower() or "fail" in soutp.lower():
-            #            log_msg(f_log, "Failure:  pip install " + line)
-            #            result.terminate()           
-            #        else:
-            #            log_msg(f_log, "Success:  pip install " + line)
-            #    else:
-            #        log_msg(f_log, "Success: conda install -c conda-forge " + line)              
-            #else:
-            #    log_msg(f_log, "Success: " +  "conda install -y --json " + line)
-
-
 #Execute command and return true for success and false for failure
 def execute_cmd(cmd, f):
+    #cmd = os.path.join(conda_path, cmd)
     log_msg(f, "Attempting: " + cmd)
     result = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
     (outp, err) = result.communicate()
@@ -119,4 +110,4 @@ def log_msg(f, msg):
 
 
 if __name__ == "__main__":
-    run_requirements("py36")
+    run_requirements(env)
